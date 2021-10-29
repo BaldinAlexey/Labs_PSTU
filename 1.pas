1727ea803274c2f9790820ecc9de60462e886ad4
Пользователь <bmrvip@mail.ru>
2021-10-29 20:35:04 +0500

    load

---
+++ 1.pas
@@ -0,0 +1,27 @@
+﻿{Программа должна вывести все возможные комбинации чисел из 3-ех значного числа и найти среди них самое большое}
+var  x:integer; 
+a, b, c, d, i, max: integer;
+begin
+ write('Введите 3-х значное число:  ');
+readln(x);
+a:=x div 100;
+b:=(x div 10) mod 10;
+c:=x mod 10;
+max:=0;
+for i:=1 to 3 do
+    begin
+ x:=100*a+10*b+c;
+ writeln(x);
+ if x>max then max:=x;
+ d:=b;
+ b:=c;
+ c:=d;
+ x:=100*a+10*b+c;
+  writeln(x);
+  if x> max then max:=x;
+d:=c;
+c:=a;
+a:=d;
+    end;
+    writeln('Максимальное число:', max);
+end.
---
+++ Ugaday_chislo.pas
@@ -0,0 +1,14 @@
+﻿{Задача отгадать число,заданное программой за 6 попыток}
+var n,i,x, k:byte;
+begin
+randomize;
+n:=random(100)+1;
+k:=0;
+repeat
+readln(x);
+k:=k+1;
+if x>n  then writeln('Меньше');
+if x<n then  writeln('Больше');
+until  x=n;
+writeln('Угадал за ', k,'  шагов!' );
+end.
